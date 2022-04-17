e0337f879cdf5674eb1dc118ba5ba7aa3388076d
Florian Martin <martinfl87@gmail.com>
2022-04-17 13:32:31 +0200

    🚚 Switch global.css to less

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { MoralisProvider } from "react-moralis";
 import { Web3Provider } from "../context/Web3Context";
 import "../styles/antd.less";
-import "../styles/globals.css";
+import "../styles/globals.less";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
