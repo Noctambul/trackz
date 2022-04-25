9d9ba91efe2d23a1ae3293c433d8acd8dbc47116
Florian Martin <fmartin@semeia.io>
2022-04-25 17:52:58 +0200

    🔥 Remove thirdweb dependency

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -8,58 +8,10 @@ import styles from "styles/Home.module.less";
 
 const Home: NextPage = () => {
   const { tokens } = useContext(Web3Context) as Web3ContextInterface;
-  // const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  // const fetchNFTsWithThirdweb = async () => {
-  //   setIsLoading(true);
-  //   const provider = await Moralis.enableWeb3();
-  //   const sdk = new ThirdwebSDK(provider);
-  //   const contract = sdk.getEdition(nftCollectionAddress);
-  //   const nfts = await contract.getAll();
-  //   const tokensMetadata = nfts.map((nft) => nft.metadata as TokenMetadata);
-  //   setTokens(tokensMetadata);
-  //   console.log(tokensMetadata);
-  //   setIsLoading(false);
-  // };
-
-  // const menu = (
-  //   <Menu>
-  //     <Menu.Item key="moralis">
-  //       <Button
-  //         size="small"
-  //         type="text"
-  //         onClick={fetchNFTsForContract}
-  //         loading={isLoading}
-  //       >
-  //         Fetch NFTs With Moralis
-  //       </Button>
-  //     </Menu.Item>
-  //     <Menu.Item key="thirdweb">
-  //       <Button
-  //         size="small"
-  //         type="text"
-  //         onClick={fetchNFTsWithThirdweb}
-  //         loading={isLoading}
-  //       >
-  //         Fetch NFTs With Thirdweb
-  //       </Button>
-  //     </Menu.Item>
-  //   </Menu>
-  // );
-
-  // useEffect(() => {
-  //   fetchNFTsForContract();
-  // });
 
   return (
     <PageContainer>
       <>
-        {/* <Dropdown overlay={menu}>
-          <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
-            <SettingOutlined />
-          </a>
-        </Dropdown> */}
-        {/* <span>{isLoading}</span> */}
         <Space
           direction="vertical"
           align="center"
