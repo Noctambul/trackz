f04d9e8267039d58b40ab3675a561de3c830bc57
Florian Martin <martinfl87@gmail.com>
2022-05-11 10:35:14 +0200

    ✨ Create mint page

--- web/components/PageContainer/PageContainer.tsx
+++ web/components/PageContainer/PageContainer.tsx
@@ -1,6 +1,8 @@
-import { Layout, Typography } from "antd";
+import { Layout, Menu, Typography } from "antd";
 import AudioPlayer from "components/AudioPlayer/AudioPlayer";
-import React from "react";
+import { Web3Context, Web3ContextInterface } from "context/Web3Context";
+import Link from "next/link";
+import React, { useContext } from "react";
 import ConnectButton from "../ConnectButton/ConnectButton";
 import styles from "./PageContainer.module.less";
 
@@ -12,12 +14,23 @@ export default function PageContainer({
 }: {
   children: React.ReactChild;
 }): JSX.Element {
+  const { currentAccount } = useContext(Web3Context) as Web3ContextInterface;
+
   return (
     <>
       <Layout>
         <Header className={styles.header}>
-          <Title>WhereIsTheMix</Title>
-          <ConnectButton />
+          <Title>
+            <Link href=".">WhereIsTheMix</Link>
+          </Title>
+          <Menu className={styles.rightMenu}>
+            {currentAccount && (
+              <Menu.Item key="mint">
+                <Link href="mint">Mint</Link>
+              </Menu.Item>
+            )}
+            <ConnectButton />
+          </Menu>
         </Header>
         <Content className={styles.content}>{children}</Content>
         <Footer className={styles.footer}>
