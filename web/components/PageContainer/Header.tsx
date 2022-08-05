3b085bc1e4d4fc51b1060514f32d36bfa9d4280e
Florian Martin <fmartin@semeia.io>
2022-08-05 14:51:27 +0200

    Setup Tailwind CSS Intellisense

--- web/components/PageContainer/Header.tsx
+++ web/components/PageContainer/Header.tsx
@@ -2,7 +2,8 @@ export default function Header(): JSX.Element {
   return (
     <header className="flex flex-1 items-center justify-around bg-bgc text-primary">
       <h1>TRACKZ</h1>
-      <div className="rounded-full border-amber-400 bg-primary w-2 h-2"></div>
+      <div className="rounded-full text- border-amber-400 bg-primary w-2 h-2"></div>
+      <div className="bg-bgc"></div>
     </header>
   );
 }
