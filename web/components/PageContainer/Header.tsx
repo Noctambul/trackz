f100623494a9a5245d0718405eccb51e6c87fa75
Florian Martin <fmartin@semeia.io>
2022-08-05 15:05:44 +0200

    💄 Set header width and height

--- web/components/PageContainer/Header.tsx
+++ web/components/PageContainer/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header(): JSX.Element {
   return (
-    <header className="flex flex-1 items-center justify-around bg-bgc text-primary">
+    <header className="flex flex-1 items-center justify-around bg-bgc text-primary w-screen h-10">
       <h1>TRACKZ</h1>
       <div className="rounded-full text- border-amber-400 bg-primary w-2 h-2"></div>
       <div className="bg-bgc"></div>
