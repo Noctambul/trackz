1067180847ac93103befa7cdbcb328419548495f
Florian Martin <martinfl87@gmail.com>
2022-08-07 07:49:14 +0200

    🚧 Test div and fixed position

--- web/components/PageContainer/PageContainer.tsx
+++ web/components/PageContainer/PageContainer.tsx
@@ -1,7 +1,4 @@
-import trackzs from "data/trackzs";
 import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
 
 export default function PageContainer({
   children,
@@ -10,9 +7,12 @@ export default function PageContainer({
 }): JSX.Element {
   return (
     <>
-      <Header />
+      {/* <Header />
       <div className="h-full bg-black">{children}</div>
-      <Footer trackzs={trackzs} />
+      <Footer trackzs={trackzs} /> */}
+      <div className="relative flex h-[550rem] bg-green-400"></div>
+      <div className="fixed top-0 left-0 flex h-10 w-full bg-red-400"></div>
+      <div className="fixed bottom-0 left-0 flex h-10 w-full bg-blue-400"></div>
     </>
   );
 }
