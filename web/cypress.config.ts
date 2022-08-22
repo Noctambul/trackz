d54d381294f7358ef9d5f512bf7539280a0d5ab6
Florian Martin <martinfl87@gmail.com>
2022-08-22 13:50:41 +0200

    ✅ Add a first e2e test

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -9,8 +9,9 @@ export default defineConfig({
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 });
