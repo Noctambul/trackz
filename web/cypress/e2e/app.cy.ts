32fcb7f45b2c59bf747da3e104d938f4753ebe86
Florian Martin <martinfl87@gmail.com>
2022-08-24 15:28:50 +0200

    🔥 Remove Cypress tests

--- web/cypress/e2e/app.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("Navigation", () => {
-  it("should display the title", () => {
-    cy.visit("/");
-    cy.get("h1").contains("TRACKZ");
-  });
-});
-
-// Prevent TypeScript from reading file as legacy script
-export {};
