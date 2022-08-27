4a47fbbbce00fe0bea4906e72ba144e4ee56865e
Florian Martin <martinfl87@gmail.com>
2022-08-27 20:24:56 +0200

    ✅ Rework navigation markdown and add test

--- web/components/PageContainer/Header.tsx
+++ web/components/PageContainer/Header.tsx
@@ -3,13 +3,19 @@ import Link from "next/link";
 export default function Header(): JSX.Element {
   return (
     <header className="fixed top-0 left-0 flex h-12 w-screen items-center justify-around bg-bgc text-primary">
-      <h1 className="text-3xl font-extrabold">
+      <h1 className="text-3xl font-extrabold" aria-label="Home Page">
         <Link href="/">TRACKZ</Link>
       </h1>
-      <div className="flex items-center gap-10 text-lg">
-        <Link href="mint">MINT</Link>
-        <div className="text- mb-1 h-4 w-4 rounded-full bg-primary"></div>
-      </div>
+      <nav>
+        <ul className="flex list-none items-center gap-10 text-lg">
+          <li aria-label="Mint Page">
+            <Link href="mint">MINT</Link>
+          </li>
+          <li>
+            <div className="text- mb-1 h-4 w-4 rounded-full bg-primary"></div>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
