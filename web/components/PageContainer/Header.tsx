099ca8a0d63ace8deb6e4219afd7b3644fd30d3e
Florian Martin <martinfl87@gmail.com>
2022-08-27 20:02:33 +0200

    🛶 Add mint and home to navigation

--- web/components/PageContainer/Header.tsx
+++ web/components/PageContainer/Header.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export default function Header(): JSX.Element {
   return (
     <header className="fixed top-0 left-0 flex h-12 w-screen items-center justify-around bg-bgc text-primary">
-      <h1 className="text-3xl font-extrabold">TRACKZ</h1>
-      <div className="text- h-4 w-4 rounded-full bg-primary"></div>
+      <h1 className="text-3xl font-extrabold">
+        <Link href="/">TRACKZ</Link>
+      </h1>
+      <div className="flex items-center gap-10 text-lg">
+        <Link href="mint">MINT</Link>
+        <div className="text- mb-1 h-4 w-4 rounded-full bg-primary"></div>
+      </div>
     </header>
   );
 }
