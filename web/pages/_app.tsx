f0ddfccc47de47733b5c09b86c068b1f728cba3d
Florian Martin <martinfl87@gmail.com>
2022-08-28 09:30:13 +0200

    📝 Remove a lost semicolon

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -6,7 +6,7 @@ import "../styles/global.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AudioProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </AudioProvider>
   );
 }
