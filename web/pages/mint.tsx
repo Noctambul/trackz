49f8d17056ab5639a6947082e1dc5c4a490e2411
Florian Martin <martinfl87@gmail.com>
2022-08-28 09:33:36 +0200

    ✨ Create skeleton of the mint page

--- web/pages/mint.tsx
+++ web/pages/mint.tsx
@@ -1,5 +1,53 @@
 import PageContainer from "components/PageContainer/PageContainer";
+import FileInput from "components/uikit/FileInput";
 
 export default function MintPage(): JSX.Element {
-  return <PageContainer>Mint</PageContainer>;
+  return (
+    <PageContainer>
+      <form className="mx-40 flex w-full flex-col gap-10">
+        <h3>Upload</h3>
+
+        {/* <label>
+          Music file
+          <input type="file" name="musicFile" />
+        </label> */}
+
+        <FileInput>Music File</FileInput>
+
+        <label>
+          Cover file
+          <input type="file" name="coverFile" />
+        </label>
+
+        <h3>Details</h3>
+
+        <label>
+          Title
+          <input type="text" name="title"></input>
+        </label>
+
+        <label>
+          Description
+          <textarea />
+        </label>
+
+        <label>
+          Tags
+          <input type="text" name="tags" />
+        </label>
+
+        <h3>Editions</h3>
+
+        <label>
+          Number of Editions
+          <input type="number" name="numberOfEditions" placeholder="10" />
+        </label>
+
+        <label>
+          Royalties
+          <input type="number" name="royalties" />
+        </label>
+      </form>
+    </PageContainer>
+  );
 }
