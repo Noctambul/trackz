86961ae4c9392c9ad99746a9beb0e084a4eca848
Florian Martin <martinfl87@gmail.com>
2022-09-06 08:03:02 +0200

    Add barrier for wrong url

--- web/components/AudioPlayer/TrackzInfos.tsx
+++ web/components/AudioPlayer/TrackzInfos.tsx
@@ -12,14 +12,16 @@ export default function TrackzInfos({ trackz }: Props): JSX.Element {
   return (
     <div className="w-md flex shrink-0 items-center overflow-hidden">
       <div className="aspect-square relative h-10 w-10">
-        <Image
-          alt={trackz.name}
-          src={resolveLink(trackz.coverUri)}
-          layout="fill"
-          // width="100%"
-          // height="100%"
-          className="text-ellipsis"
-        />
+        {trackz.coverUri && (
+          <Image
+            alt={trackz.name}
+            src={resolveLink(trackz.coverUri)}
+            layout="fill"
+            // width="100%"
+            // height="100%"
+            className="text-ellipsis"
+          />
+        )}
       </div>
       <div className="ml-3 flex w-40 flex-col overflow-hidden pl-4">
         <span className="truncate text-sm text-text" aria-label="Title">
--- web/hooks/useIpfs.ts
+++ web/hooks/useIpfs.ts
@@ -1,11 +1,21 @@
 import { ipfsProviderUri } from "lib/environment";
 
+export function isValidUri(url?: string | null): boolean {
+  return (
+    url !== undefined &&
+    url !== null &&
+    (url.includes("ipfs://") ||
+      url.includes("http://") ||
+      url.includes("https://"))
+  );
+}
+
 export function resolveLink(url?: string | null) {
-  if (!url) return "";
+  if (!url || !isValidUri(url)) return "";
   if (!url.includes("ipfs://")) return url;
   return url.replace("ipfs://", ipfsProviderUri);
 }
 
 export function useIpfs() {
-  return { resolveLink };
+  return { resolveLink, isValidUri };
 }
