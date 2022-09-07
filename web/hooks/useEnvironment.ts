cbdcfae80cd26c9b0aa5a5393095fdfbf420298a
Florian Martin <martinfl87@gmail.com>
2022-09-07 22:48:51 +0200

    Adding log on environment variable validations

--- web/hooks/useEnvironment.ts
+++ web/hooks/useEnvironment.ts
@@ -4,13 +4,11 @@ export default function useEnvironment() {
   // Set the variable to optional for the moment because Nextjs prerender the page and it seems that there is undefined env variables
   // @see https://vercel.com/noctambul/trackz/8XXumcPsUWm314iyn35M2tarazo1
   const trackzEditionContract = z
-    .string()
+    .string({ description: "NEXT_PUBLIC_TRACKZ_EDITION_CONTRACT" })
     .regex(/^0x[a-fA-F0-9]{40}$/g)
-    .optional()
     .parse(process.env.NEXT_PUBLIC_TRACKZ_EDITION_CONTRACT);
   const ipfsProviderUri = z
-    .string()
-    .optional()
+    .string({ description: "NEXT_PUBLIC_IPFS_PROVIDER_URI" })
     .parse(process.env.NEXT_PUBLIC_IPFS_PROVIDER_URI);
 
   return { trackzEditionContract, ipfsProviderUri };
