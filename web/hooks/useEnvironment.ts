62717b505901ac3ebf8f19aa177e9311046d5081
Florian Martin <martinfl87@gmail.com>
2022-09-08 07:45:03 +0200

    🐛 Remove unused environment variable and add description about env not defined errors

--- web/hooks/useEnvironment.ts
+++ web/hooks/useEnvironment.ts
@@ -4,12 +4,18 @@ export default function useEnvironment() {
   // Set the variable to optional for the moment because Nextjs prerender the page and it seems that there is undefined env variables
   // @see https://vercel.com/noctambul/trackz/8XXumcPsUWm314iyn35M2tarazo1
   const trackzEditionContract = z
-    .string({ description: "NEXT_PUBLIC_TRACKZ_EDITION_CONTRACT" })
+    .string({
+      required_error:
+        "NEXT_PUBLIC_TRACKZ_EDITION_CONTRACT is required as an environment variable",
+    })
     .regex(/^0x[a-fA-F0-9]{40}$/g)
     .parse(process.env.NEXT_PUBLIC_TRACKZ_EDITION_CONTRACT);
-  const ipfsProviderUri = z
-    .string({ description: "NEXT_PUBLIC_IPFS_PROVIDER_URI" })
-    .parse(process.env.NEXT_PUBLIC_IPFS_PROVIDER_URI);
+  // const ipfsProviderUri = z
+  //   .string({
+  //     required_error:
+  //       "NEXT_PUBLIC_IPFS_PROVIDER_URI is required as an environment variable",
+  //   })
+  //   .parse(process.env.NEXT_PUBLIC_IPFS_PROVIDER_URI);
 
-  return { trackzEditionContract, ipfsProviderUri };
+  return { trackzEditionContract };
 }
