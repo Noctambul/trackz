c4a2900af46adc41805ca9b6e6195f1f3f15c30f
Florian Martin <martinfl87@gmail.com>
2022-09-09 14:15:36 +0200

    📌 Add Chakra-UI dependency

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { AudioProvider } from "context/AudioContext";
 import { Web3Provider } from "context/Web3Context";
@@ -6,24 +7,28 @@ import type { AppProps } from "next/app";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  /* https://github.com/thirdweb-dev/js/tree/main/packages/react#advanced-configuration */
+  const thirdwebProps = {
+    desiredChainId: ChainId.Rinkeby,
+    dAppMeta: {
+      name: "Trackz",
+      description: "Your sound on chain",
+      logoUrl: "",
+      url: "",
+    },
+    // storageInterface={new IpfsStorage("https://your.ipfs.host.com")}
+  };
+
   return (
-    // https://github.com/thirdweb-dev/js/tree/main/packages/react#advanced-configuration
-    <ThirdwebProvider
-      desiredChainId={ChainId.Rinkeby}
-      dAppMeta={{
-        name: "Trackz",
-        description: "Your sound on chain",
-        logoUrl: "",
-        url: "",
-      }}
-      // storageInterface={new IpfsStorage("https://your.ipfs.host.com")}
-    >
-      <Web3Provider>
-        <AudioProvider>
-          <Component {...pageProps} />
-        </AudioProvider>
-      </Web3Provider>
-    </ThirdwebProvider>
+    <ChakraProvider>
+      <ThirdwebProvider {...thirdwebProps}>
+        <Web3Provider>
+          <AudioProvider>
+            <Component {...pageProps} />
+          </AudioProvider>
+        </Web3Provider>
+      </ThirdwebProvider>
+    </ChakraProvider>
   );
 }
 
