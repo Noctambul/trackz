3dcdcc4584aad2282198c7534b7f4b23bec9d93f
Florian Martin <martinfl87@gmail.com>
2022-09-09 21:18:44 +0200

    💄 Use chakra heading

--- web/components/PageContainer/Header.tsx
+++ web/components/PageContainer/Header.tsx
@@ -1,15 +1,13 @@
+import { Heading } from "@chakra-ui/react";
 import ConnectButton from "components/Thirdweb/ConnectButton";
 import Link from "next/link";
 
 export default function Header(): JSX.Element {
   return (
     <header className="fixed top-0 left-0 flex h-12 w-screen items-center justify-between bg-bgc px-10 text-white">
-      <h1
-        className="text-lg font-extrabold text-primary"
-        aria-label="Home Page"
-      >
+      <Heading as="h1" fontSize="2xl" aria-label="Home Page" color="primary">
         <Link href="/">TRACKZ</Link>
-      </h1>
+      </Heading>
       <nav>
         <ul className="flex list-none items-center gap-10">
           <li aria-label="Mint Page">
