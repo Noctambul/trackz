7c6f7055911ef96fc31ee2bbd438bdbaf3264caf
Florian Martin <martinfl87@gmail.com>
2022-09-11 07:22:52 +0200

    💄 Add animation to uikit/IconButton

--- web/components/uikit/IconButton.tsx
+++ web/components/uikit/IconButton.tsx
@@ -1,15 +1,24 @@
+import { motion, type HTMLMotionProps } from "framer-motion";
 import { IconType } from "react-icons";
 
 interface Props
-  extends Omit<React.HTMLProps<HTMLButtonElement>, "type" | "size"> {
+  extends Omit<
+    React.PropsWithChildren<HTMLMotionProps<"button">>,
+    "type" | "size"
+  > {
   Icon: IconType;
   size?: string;
 }
 
 export default function IconButton({ Icon, ...props }: Props): JSX.Element {
   return (
-    <button type="button" {...props}>
+    <motion.button
+      type="button"
+      {...props}
+      whileHover={!props.disabled ? { scale: 1.05 } : {}}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
+    >
       <Icon className={`fill-inherit stroke-inherit text-inherit`} />
-    </button>
+    </motion.button>
   );
 }
