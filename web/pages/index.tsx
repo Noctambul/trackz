2a22ff06c2eec898e090cff1ae991af284ed1255
Florian Martin <martinfl87@gmail.com>
2022-09-13 22:22:43 +0200

    💄 Add centered loading on index page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,5 @@
+import { Container, Spinner } from "@chakra-ui/react";
 import PageContainer from "components/PageContainer/PageContainer";
-import TrackzCard from "components/TrackzCard/TrackzCard";
 import { useAudio } from "context/AudioContext";
 import type { NextPage } from "next";
 
@@ -10,19 +10,23 @@ const Home: NextPage = () => {
   return (
     <PageContainer>
       <div className="mx-14 flex w-full flex-col space-y-10">
-        {playlist.length > 0
-          ? playlist.map((track) => (
-              <TrackzCard
-                trackz={track}
-                key={track.id}
-                play={play}
-                pause={pause}
-                isPlaying={isPlaying}
-                trackProgress={trackProgress}
-                isSelected={currentTrackz == track}
-              />
-            ))
-          : "Loading ..."}
+        {playlist.length > 0 ? (
+          playlist.map((track) => (
+            <TrackzCard
+              trackz={track}
+              key={track.id}
+              play={play}
+              pause={pause}
+              isPlaying={isPlaying}
+              trackProgress={trackProgress}
+              isSelected={currentTrackz == track}
+            />
+          ))
+        ) : (
+          <Container centerContent h="500px" style={{ display: "flex" }}>
+            <Spinner size="xl" />
+          </Container>
+        )}
 
         {/* {trackzs.map((trackz) => (
           <TrackzCard trackz={trackz} key={trackz.id} />
