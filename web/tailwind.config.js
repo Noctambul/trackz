ec0f9dccff132222cf8b95f7f90dbe33cdb8fe81
Florian Martin <martinfl87@gmail.com>
2022-09-15 21:42:20 +0200

    Use lightgray theme color

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,13 +5,6 @@ module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      // colors: {
-      //   primary: colors.zinc[900], //"#EA8C04",
-      //   bgc: colors.white, //colors.zinc[900],
-      //   text: colors.zinc[900], //colors.gray[100],
-      //   subtext: colors.gray[600],
-      //   // bgc: colors.red[400],
-      // },
       colors: {
         primary: "#F3B2A6",
         bgc: "#1E2336",
