4b7ee50335a8db4017f27cc1a5a2380e48d2e6ed
Florian Martin <martinfl87@gmail.com>
2022-09-17 18:48:49 +0200

    ✨ Create device detection hook

---
+++ web/hooks/useDeviceDetection.ts
@@ -0,0 +1,15 @@
+import { useMemo } from "react";
+
+export default function useDeviceDetection() {
+  const isMobile = useMemo(() => {
+    if (typeof window.navigator === "undefined") return;
+
+    return Boolean(
+      navigator.userAgent.match(
+        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+      )
+    );
+  }, []);
+
+  return { isMobile };
+}
