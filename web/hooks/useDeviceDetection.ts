38b180f746302af81ab6a156034106128c049460
Florian Martin <martinfl87@gmail.com>
2022-09-17 19:08:17 +0200

    💄 Increase TrackzCard cover

--- web/hooks/useDeviceDetection.ts
+++ web/hooks/useDeviceDetection.ts
@@ -1,6 +1,10 @@
 import { useMemo } from "react";
 
 export default function useDeviceDetection() {
+  /**
+   * TODO: Try useing useBreakpointValue
+   * @see https://chakra-ui.com/docs/hooks/use-breakpoint-value#import
+   */
   const isMobile = useMemo(() => {
     if (
       typeof window === "undefined" ||
