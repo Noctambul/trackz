cb81346da047e9ee803b185f79e31e14ba4ee9a0
Florian Martin <martinfl87@gmail.com>
2022-09-17 12:33:28 +0200

    💄 Volume track is now displayed on hover

--- web/components/AudioPlayer/Volume.tsx
+++ web/components/AudioPlayer/Volume.tsx
@@ -1,9 +1,12 @@
 import { useAudio } from "context/AudioContext";
 
 import {
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
   Slider,
   SliderFilledTrack,
-  SliderThumb,
   SliderTrack,
 } from "@chakra-ui/react";
 import { TbVolume, TbVolume2, TbVolume3 } from "react-icons/tb";
@@ -14,28 +17,44 @@ export default function Volume({}): JSX.Element {
   const VolumeIcon = isMuted ? TbVolume3 : volume > 0.4 ? TbVolume : TbVolume2;
 
   return (
-    <div className="ml-2 flex items-center justify-center pl-2">
-      <button type="button" onClick={toggleMute}>
-        <VolumeIcon
-          className="mr-2 text-2xl"
-          aria-label={isMuted ? "Unmute" : "Mute"}
-        />
-      </button>
-
-      <Slider
-        aria-label="Track volume"
-        className="mx-2"
-        min={0}
-        max={100}
-        value={volume * 100}
-        onChange={(vol) => setVolume(vol / 100)}
-        w={20}
-      >
-        <SliderTrack bg="blue.100" boxSize={0.5}>
-          <SliderFilledTrack bg="primary" />
-        </SliderTrack>
-        <SliderThumb />
-      </Slider>
-    </div>
+    <>
+      <Popover offset={[-6, 0]} placement="top" trigger="hover">
+        <PopoverTrigger>
+          <button type="button" onClick={toggleMute}>
+            <VolumeIcon
+              className="mr-2 text-2xl"
+              aria-label={isMuted ? "Unmute" : "Mute"}
+            />
+          </button>
+        </PopoverTrigger>
+        <PopoverContent
+          bg="bgc"
+          border={0}
+          w={12}
+          pt={4}
+          pb={1}
+          borderRadius={0}
+        >
+          <PopoverBody className="flex items-center justify-center">
+            <Slider
+              aria-label="Track volume"
+              min={0}
+              max={100}
+              value={volume * 100}
+              onChange={(vol) => setVolume(vol / 100)}
+              h={28}
+              orientation="vertical"
+            >
+              <SliderTrack boxSize={3} borderRadius="1.75rem">
+                <SliderFilledTrack bg="primary" />
+              </SliderTrack>
+            </Slider>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+      {/*       
+      <div className="ml-2 flex items-center justify-center pl-2">
+      </div> */}
+    </>
   );
 }
