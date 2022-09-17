629250fc080bdc5efb0c57a9b2d5382bfca2b158
Florian Martin <martinfl87@gmail.com>
2022-09-17 06:21:42 +0200

    ✅ Test mint form submition

--- web/cypress/e2e/Mint.cy.ts
+++ web/cypress/e2e/Mint.cy.ts
@@ -1,12 +1,35 @@
 describe("Mint Page", () => {
-  before(() => cy.intercept("GET", "/api/trackzs", { editions: [] }));
+  before(() => {
+    cy.intercept("GET", "/api/trackzs", { editions: [] }).as("getTrackzs");
+    cy.intercept("POST", "api/mint", cy.spy().as("postMintSpy")).as("postMint");
+  });
 
-  describe("if the wallet is not connected", () => {
+  beforeEach(() => cy.visit("/mint"));
+
+  context("when the wallet is not connected", () => {
     it("displays a connect wallet button", () => {
-      cy.visit("/mint");
       cy.get(`[aria-label="Mint"]`).should("not.exist");
-      cy.get(`[aria-label="Connect wallet"]`).should("be.visible");
+      cy.get(`form button[aria-label="Connect wallet"]`)
+        .should("be.visible")
+        .click();
+      cy.get(`[aria-label="Mint"]`).should("be.visible");
     });
+  });
+
+  context.only("with a connected wallet", () => {
+    const submit = () =>
+      cy.get(`button[type="submit"][aria-label="Mint"]`).click();
+
+    beforeEach(() =>
+      cy.get(`form button[aria-label="Connect wallet"]`).click()
+    );
+
+    it("can't submit without filling fields", () => {
+      submit();
+      cy.get("@postMintSpy").should("not.have.been.called");
+    });
+
+    it("can fullfill the form", () => {});
   });
 });
 
