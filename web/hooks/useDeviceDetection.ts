e9fb309193267cad71bfdbf3f695174a9d0e2131
Florian Martin <martinfl87@gmail.com>
2022-09-17 19:01:59 +0200

    🐛 Do not use window on server side

--- web/hooks/useDeviceDetection.ts
+++ web/hooks/useDeviceDetection.ts
@@ -2,7 +2,11 @@ import { useMemo } from "react";
 
 export default function useDeviceDetection() {
   const isMobile = useMemo(() => {
-    if (typeof window.navigator === "undefined") return;
+    if (
+      typeof window === "undefined" ||
+      typeof window.navigator === "undefined"
+    )
+      return;
 
     return Boolean(
       navigator.userAgent.match(
