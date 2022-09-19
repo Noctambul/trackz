c4fc77864c74ad7490e7d87583c23186b4036bc9
Florian Martin <fmartin@semeia.io>
2022-09-19 12:57:41 +0200

    🔁 Revert to commit💄 Add custom favicon - a64a5e3 - Florian Martin <martinfl87@gmail.com - 19 Sep 2022 at 09:04

--- web/lib/chakra-theme.ts
+++ web/lib/chakra-theme.ts
@@ -4,14 +4,14 @@ import tailwindConfig from "../tailwind.config";
 
 const tailwind = resolveConfig(tailwindConfig);
 const colors = tailwind!.theme!.colors!;
-// const fonts = tailwind!.theme!.fontFamily!;
+const fonts = tailwind!.theme!.fontFamily!;
 
 const theme = extendTheme({
   colors,
-  // fonts: {
-  //   heading: `"KumbhSans", sans-serif`,
-  //   body: `"KumbhSans", sans-serif`,
-  // },
+  fonts: {
+    heading: `"KumbhSans", sans-serif`,
+    body: `"KumbhSans", sans-serif`,
+  },
 });
 
 export default theme;
