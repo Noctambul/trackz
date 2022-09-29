1349faebd1a3dfb05de723bf316fc6c3a2f74f6c
Florian Martin <martinfl87@gmail.com>
2022-09-29 07:16:10 +0200

    ✨(trackz-card): Add play and add to playlist buttons

--- web/common/components/TrackzCard/TrackzCard.tsx
+++ web/common/components/TrackzCard/TrackzCard.tsx
@@ -1,25 +1,83 @@
-import { useIpfs } from "common/hooks/useIpfs";
+import { IconButton as ChakraIconButton } from "@chakra-ui/react";
 import AudioTrackz from "modules/audio/models/AudioTrackz";
 import Image from "next/image";
 import Link from "next/link";
+import { RiPauseFill, RiPlayFill, RiPlayListAddFill } from "react-icons/ri";
 
 type Props = {
   track: AudioTrackz;
+  /** Is the given trackz the one currently selected by the player */
+  isSelected: boolean;
+  /** Is the player playing a track */
+  isPlaying: boolean;
+  play: (trackz: AudioTrackz) => void;
+  pause: () => void;
+  addToPlaylist: (track: AudioTrackz) => void;
 };
 
-export default function TrackzCard({ track }: Props): JSX.Element {
-  const { resolveLink } = useIpfs();
+export default function TrackzCard({
+  track,
+  play,
+  pause,
+  isSelected,
+  isPlaying,
+  addToPlaylist,
+}: Props): JSX.Element {
+  const iconSize = 20;
+  const iconBtnStyle =
+    "m-1 opacity-0 transition-opacity group-hover:opacity-80";
+
+  const isPlayingMe = () => isPlaying && isSelected;
+
+  const PlayChakraButton = (
+    <ChakraIconButton
+      className={iconBtnStyle}
+      aria-label={`Play ${track.name}`}
+      isRound={true}
+      icon={
+        isPlayingMe() ? (
+          <RiPauseFill size={iconSize} />
+        ) : (
+          <RiPlayFill size={iconSize} />
+        )
+      }
+      onClick={(e) => {
+        e.preventDefault();
+        isPlayingMe() ? pause() : play(track);
+      }}
+      size="lg"
+    />
+  );
+
+  const AddToPlaylistBtn = (
+    <ChakraIconButton
+      className={iconBtnStyle}
+      icon={<RiPlayListAddFill size={iconSize} />}
+      aria-label={`Add ${track.name} to playlist`}
+      colorScheme="whiteAlpha"
+      size="lg"
+      isRound={true}
+      onClick={(e) => {
+        e.preventDefault();
+        addToPlaylist(track);
+      }}
+    />
+  );
 
   return (
     <div className="flex h-full w-full max-w-sm  flex-col items-center rounded-lg bg-white py-2 shadow-md">
       <Link href={`trackzs/${track.id}`}>
-        <div className="aspect-square relative h-full w-[calc(100%-0.9rem)] cursor-pointer overflow-hidden rounded-md border border-lightgray">
+        <div className="aspect-square group relative h-full w-[calc(100%-0.9rem)] cursor-pointer overflow-hidden rounded-md border border-lightgray">
           <Image
             src={track.coverUri}
             alt={track.name}
             layout="fill"
             objectFit="cover"
           />
+          <div className="absolute bottom-0 flex w-full justify-between gap-2">
+            {PlayChakraButton}
+            {AddToPlaylistBtn}
+          </div>
         </div>
       </Link>
       <div className="my-2 w-full px-2">
