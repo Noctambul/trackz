90aab11536632e3de0aa8dc427ff8e08fbd4ee05
Florian Martin <martinfl87@gmail.com>
2022-10-02 11:37:16 +0200

    ✅❗️(playlist): Creating failing test to cover playlist bugs

--- web/cypress/e2e/AudioPlayer.cy.ts
+++ web/cypress/e2e/AudioPlayer.cy.ts
@@ -35,6 +35,12 @@ describe("Audio Player", () => {
     cy.get(`[aria-label="Trackz Card ${trackzs[index].id}"]`);
   const playlistBtn = () => player().get(`[aria-label="Playlist Button"]`);
   const playlistContainer = () => player().get(`[aria-label="Playlist"]`);
+  const playlistEltAt = (index: number) =>
+    cy.get(`[aria-label="Playlist"] [data-playlist-index="${index}"]`);
+  const addToPlaylist = (index: number) =>
+    cy.get(`[aria-label="Add ${trackzs[index].name} to playlist"]`);
+  const clearPlaylistBtn = () =>
+    playlistContainer().get(`[aria-label="Clear Playlist"]`);
   const playlistTrack = (index: number) =>
     playlistContainer().get(`[aria-label="Track ${trackzs[index].name}"]`);
   const playlistTrackRemoveBtn = (index: number) =>
@@ -65,7 +71,7 @@ describe("Audio Player", () => {
     volBtn().click();
   });
 
-  context("from the home", () => {
+  context("From the home", () => {
     it("displays the tracker with the first track selected", () => {
       player().should("exist");
       player().should("be.visible").log("The Audio Player is visible");
@@ -73,7 +79,7 @@ describe("Audio Player", () => {
     });
   });
 
-  context("audio controlls", () => {
+  context("Audio controlls", () => {
     it("can switch between trackz", () => {
       prevBtn().should("be.disabled");
       nextBtn().should("be.enabled");
@@ -117,7 +123,7 @@ describe("Audio Player", () => {
     });
   });
 
-  context("trackz cards", () => {
+  context("Trackz cards", () => {
     it("can play a track from a card", () => {
       playBtn().should("exist");
       playTrackBtn(2).click();
@@ -138,110 +144,137 @@ describe("Audio Player", () => {
     });
   });
 
-  context("playlist", () => {
-    it("shows and hides by clicking the button", () => {
-      playlistContainer().should("not.be.visible");
-      playlistBtn().click();
-      playlistContainer().should("be.visible");
-      playlistBtn().click();
-      playlistContainer().should("not.be.visible");
-    });
-
-    it("can play and pause a track", () => {
-      playlistBtn().click();
-      playlistTrack(1).click();
-      playlistTrack(1).get(`[data-test-pause]`).should("be.visible");
-      pauseBtn().should("be.visible");
-      playBtn().should("not.exist");
-      playlistTrack(1).click();
-      playlistTrack(1).get(`[data-test-play]`).should("be.visible");
-      pauseBtn().should("not.exist");
-      playBtn().should("be.visible");
-      playlistTrack(2).click();
-      playlistTrack(2).get(`[data-test-pause]`).should("be.visible");
-      playlistTrack(0).click();
-      playlistTrack(0).get(`[data-test-pause]`).should("be.visible");
-    });
-
-    it("has the same track selected than the player", () => {
-      playlistBtn().click();
-      playlistTrack(0).get(`[data-test-play]`).should("be.visible");
-      shouldHaveTrack(0);
-      nextBtn().click();
-      playlistTrack(0).get(`[data-test-play]`).should("not.be.visible");
-      playlistTrack(1).get(`[data-test-pause]`).should("be.visible");
-      shouldHaveTrack(1);
-      pauseBtn().click();
-      playlistTrack(1).get(`[data-test-play]`).should("be.visible");
-    });
-
-    it("has a disabled style on the previous trackzs", () => {
-      playlistBtn().click();
-      playlistTrack(2).click();
-      playlistTrack(0).get(`img`).should("have.class", "opacity-40");
-      playlistTrack(1).get(`img`).should("have.class", "opacity-40");
-      playlistTrack(0).click();
-      playlistTrack(0).get(`img`).should("not.have.class", "opacity-40");
-      playlistTrack(1).get(`img`).should("not.have.class", "opacity-40");
-    });
-
-    it("removes a track from the playlist", () => {
-      playlistBtn().click().log("Open the playlist");
-      playlistTrack(1)
-        .should("exist")
-        .log(`Trackz '${trackzs[1].name}' exists in the playlist`);
-      playlistTrackRemoveBtn(1)
-        .click()
-        .log(`Remove track '${trackzs[1].name}' from the playlist`);
-      trackzCardElt(1)
-        .should("exist")
-        .log(`TrackzCard '${trackzs[1].name}' still exists`);
-      playlistTrack(1)
-        .should("not.exist")
-        .log(
-          `Trackz '${trackzs[1].name}' does not exists in the playlist anymore`
-        );
-      playlistTrack(0)
-        .should("exist")
-        .log(`Trackz ${trackzs[0].name} exists in the playlist`);
-      playlistTrackRemoveBtn(0)
-        .click()
-        .log(`Remove track '${trackzs[1].name}' from the playlist`);
-      trackzCardElt(0)
-        .should("exist")
-        .log(`TrackzCard '${trackzs[0].name}' still exists`);
-      playlistTrack(0)
-        .should("not.exist")
-        .log(
-          `Trackz '${trackzs[0].name}' does not exists in the playlist anymore`
-        );
-      playlistTrack(2)
-        .get(`[data-test-play="${trackzs[2].id}"]`)
+  context("Playlist", () => {
+    const isTrackPlaying = (index: number) =>
+      cy
+        .get(`[data-playlist-index="${index}"] [data-test-pause]`)
         .should("be.visible");
-      shouldHaveTrack(2);
+
+    context("Navigating", () => {
+      it("shows and hides by clicking the button", () => {
+        playlistContainer().should("not.be.visible");
+        playlistBtn().click();
+        playlistContainer().should("be.visible");
+        playlistBtn().click();
+        playlistContainer().should("not.be.visible");
+      });
+
+      it("can play and pause a track", () => {
+        playlistBtn().click();
+        playlistTrack(1).click();
+        playlistTrack(1).get(`[data-test-pause]`).should("be.visible");
+        pauseBtn().should("be.visible");
+        playBtn().should("not.exist");
+        playlistTrack(1).click();
+        playlistTrack(1).get(`[data-test-play]`).should("be.visible");
+        pauseBtn().should("not.exist");
+        playBtn().should("be.visible");
+        playlistTrack(2).click();
+        playlistTrack(2).get(`[data-test-pause]`).should("be.visible");
+        playlistTrack(0).click();
+        playlistTrack(0).get(`[data-test-pause]`).should("be.visible");
+      });
+
+      it("has the same track selected than the player", () => {
+        playlistBtn().click();
+        playlistTrack(0).get(`[data-test-play]`).should("be.visible");
+        shouldHaveTrack(0);
+        nextBtn().click();
+        playlistTrack(0).get(`[data-test-play]`).should("not.be.visible");
+        playlistTrack(1).get(`[data-test-pause]`).should("be.visible");
+        shouldHaveTrack(1);
+        pauseBtn().click();
+        playlistTrack(1).get(`[data-test-play]`).should("be.visible");
+      });
+
+      it("has a disabled style on the previous trackzs", () => {
+        playlistBtn().click();
+        playlistTrack(2).click();
+        playlistTrack(0).get(`img`).should("have.class", "opacity-40");
+        playlistTrack(1).get(`img`).should("have.class", "opacity-40");
+        playlistTrack(0).click();
+        playlistTrack(0).get(`img`).should("not.have.class", "opacity-40");
+        playlistTrack(1).get(`img`).should("not.have.class", "opacity-40");
+      });
+
+      it("can select precisely the right index between several identical Trackz", () => {
+        playlistBtn().click();
+        addToPlaylist(1).click().click().click();
+        playlistEltAt(4).click();
+        isTrackPlaying(4);
+      });
     });
 
-    it("removes a track that is before the current selected one", () => {
-      playlistBtn().click();
-      playlistTrack(2).click();
-      shouldHaveTrack(2);
-      playlistTrackRemoveBtn(0).click();
-      shouldHaveTrack(2);
-      pauseBtn().should("be.visible");
-      playlistTrackRemoveBtn(1).click();
-      shouldHaveTrack(2);
-      pauseBtn().should("be.visible");
+    context("Removing", () => {
+      it("removes a track from the playlist", () => {
+        playlistBtn().click().log("Open the playlist");
+        playlistTrack(1)
+          .should("exist")
+          .log(`Trackz '${trackzs[1].name}' exists in the playlist`);
+        playlistTrackRemoveBtn(1)
+          .click()
+          .log(`Remove track '${trackzs[1].name}' from the playlist`);
+        trackzCardElt(1)
+          .should("exist")
+          .log(`TrackzCard '${trackzs[1].name}' still exists`);
+        playlistTrack(1)
+          .should("not.exist")
+          .log(
+            `Trackz '${trackzs[1].name}' does not exists in the playlist anymore`
+          );
+        playlistTrack(0)
+          .should("exist")
+          .log(`Trackz ${trackzs[0].name} exists in the playlist`);
+        playlistTrackRemoveBtn(0)
+          .click()
+          .log(`Remove track '${trackzs[1].name}' from the playlist`);
+        trackzCardElt(0)
+          .should("exist")
+          .log(`TrackzCard '${trackzs[0].name}' still exists`);
+        playlistTrack(0)
+          .should("not.exist")
+          .log(
+            `Trackz '${trackzs[0].name}' does not exists in the playlist anymore`
+          );
+        playlistTrack(2)
+          .get(`[data-test-play="${trackzs[2].id}"]`)
+          .should("be.visible");
+        shouldHaveTrack(2);
+      });
+
+      it("removes a track that is before the current selected one", () => {
+        playlistBtn().click();
+        playlistTrack(2).click();
+        shouldHaveTrack(2);
+        playlistTrackRemoveBtn(0).click();
+        shouldHaveTrack(2);
+        pauseBtn().should("be.visible");
+        playlistTrackRemoveBtn(1).click();
+        shouldHaveTrack(2);
+        pauseBtn().should("be.visible");
+      });
     });
 
-    it("reset the playlist when playing a track that is not in the playlist", () => {
-      playlistBtn().click();
-      playlistTrackRemoveBtn(1).click();
-      playTrackBtn(1).click();
-      shouldHaveTrack(1);
-      playlistTrack(1).should("be.visible");
-      playlistTrack(0).should("not.exist");
-      playlistTrack(2).should("not.exist");
-      pauseBtn().should("be.visible");
+    context("Reseting", () => {
+      it("reset the playlist when playing a track that is not in the playlist", () => {
+        playlistBtn().click();
+        playlistTrackRemoveBtn(1).click();
+        playTrackBtn(1).click();
+        shouldHaveTrack(1);
+        playlistTrack(1).should("be.visible");
+        playlistTrack(0).should("not.exist");
+        playlistTrack(2).should("not.exist");
+        pauseBtn().should("be.visible");
+      });
+
+      it("can clear the playlist and keep the current selected Trackz in it", () => {
+        playlistBtn().click();
+        playlistTrack(2).click();
+        clearPlaylistBtn().click();
+        player().should("be.visible");
+        playlistContainer().should("be.visible");
+        isTrackPlaying(0);
+      });
     });
   });
 });
