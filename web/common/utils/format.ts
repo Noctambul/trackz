9a8aae6a8cc29fceaf42ddfb15842ecc8d729b04
Florian Martin <martinfl87@gmail.com>
2022-10-02 10:08:46 +0200

    🎨(utils): Improve formatWallet

--- web/common/utils/format.ts
+++ web/common/utils/format.ts
@@ -1,4 +1,11 @@
+import EthWalletAddressSchema from "lib/schema/eth-wallet-address-schema";
+
 export function formatWallet(wallet: string | undefined): string {
   if (!wallet || !Boolean(wallet)) return "";
-  return `${wallet.substring(0, 5)}...${wallet.substring(wallet.length - 4)}`;
+  const parsed = EthWalletAddressSchema.safeParse(wallet);
+  return parsed.success
+    ? `${parsed.data.substring(0, 5)}...${parsed.data.substring(
+        parsed.data.length - 4
+      )}`
+    : wallet;
 }
