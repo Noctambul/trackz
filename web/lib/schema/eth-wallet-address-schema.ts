844e38e1b60f3338b7154c6a1be361e43a973de8
Florian Martin <martinfl87@gmail.com>
2022-10-05 07:14:43 +0200

    📌 Adding validator-js

--- web/lib/schema/eth-wallet-address-schema.ts
+++ web/lib/schema/eth-wallet-address-schema.ts
@@ -1,6 +1,10 @@
+import isEthereumAddress from "validator/lib/isEthereumAddress";
 import { z } from "zod";
 
-const EthWalletAddressSchema = z.string().regex(/^0x[a-fA-F0-9]{40}$/g);
+// const EthWalletAddressSchema = z.string().regex(/^0x[a-fA-F0-9]{40}$/g);
+const EthWalletAddressSchema = z
+  .string()
+  .refine(isEthereumAddress, { message: "Must be an ethereum address" });
 
 export default EthWalletAddressSchema;
 
